a61641e9f6b6135d47befb359610f5cff4a6996d
mbthiery <mbthiery@Matthews-MacBook-Pro.local>
2015-11-02 13:39:53 -0500

    Reverted change

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,9 +1,15 @@
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
-module.exports = {
-  User: require('./models.user'),
-  Product: require('./models.product'),
-  Order: require('./models.order'),
-  Review: require('./models.review')
-};
+
+require('./models.user');
+require('./models.product');
+require('./models.order');
+require('./models.review');
+
+// module.exports = {
+//   User: require('./models.user'),
+//   Product: require('./models.product'),
+//   Order: require('./models.order'),
+//   Review: require('./models.review')
+// };
