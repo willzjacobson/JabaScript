a9609537f1f7d01f1b9e3f9daf6ede2ac516ff4d
mbthiery <mbthiery@Matthews-MacBook-Pro.local>
2015-11-02 12:24:05 -0500

    model/api framework setup

---
+++ server/app/routes/api/index.js
@@ -0,0 +1,8 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+router.use('/users', require('./api.users'));
+router.use('/products', require('./api.products'));
+router.use('/orders', require('./api.orders'));
+router.use('/reviews', require('./api.reviews'));
