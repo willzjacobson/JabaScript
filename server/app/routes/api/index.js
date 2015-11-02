7bf6b32c2e8f0bd757a6b158753eab0342815834
mbthiery <mbthiery@Matthews-MacBook-Pro.local>
2015-11-02 14:24:03 -0500

    Fixed router issue

--- server/app/routes/api/index.js
+++ server/app/routes/api/index.js
@@ -2,7 +2,8 @@
 var router = require('express').Router();
 module.exports = router;
 
+
 router.use('/users', require('./api.users'));
-router.use('/products', require('./api.products'));
-router.use('/orders', require('./api.orders'));
-router.use('/reviews', require('./api.reviews'));
+// router.use('/products', require('./api.products'));
+// router.use('/orders', require('./api.orders'));
+// router.use('/reviews', require('./api.reviews'));
