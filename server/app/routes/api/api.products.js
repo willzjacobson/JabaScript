87ce8372677f9d9219aa3cf5c476352b4b210ad3
Samuel Ballan <sgb4622@gmail.com>
2015-11-02 13:07:43 -0500

    pre lunch commit

--- server/app/routes/api/api.products.js
+++ server/app/routes/api/api.products.js
@@ -1,3 +1,29 @@
 var router = require('express').Router();
-var 
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var Product = require(__dirname + 'server/db/models').Product;
+Promise.promisify(mongoose);
+
+console.log(Product)
+
 module.exports = router;
+
+router.get('/', function(req, res, next) {
+
+});
+
+router.get('/:id', function(req, res, next) {
+
+});
+
+router.post('/', function(req, res, next) {
+
+});
+
+router.put('/:id', function(req, res, next) {
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+});
