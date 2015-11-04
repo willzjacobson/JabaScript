a034d09f4e41eadfbfe583d0f59e9112715d4fdb
Samuel Ballan <sgb4622@gmail.com>
2015-11-04 15:38:22 -0500

    Updated factories and standardized schemas

--- server/db/models/models.order.js
+++ server/db/models/models.order.js
@@ -20,10 +20,30 @@ var orderSchema = new mongoose.Schema({
       ref: 'User',
       required: true
     },
+    // toObject: {virtuals: true },
+    // toJSON: {virtuals: true},
     items: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Item'
     }]
 });
+
+//TODO We don't know if this works; test when we have a front end
+orderSchema.methods.getTotalPrice = function()  {
+  var promiseForItems = this.items.map(function(itemId) {
+    return mongoose.model('Item').find({_id: this._id});
+  })
+  return Promise.all(promiseForItems).then(function(theItems) {
+    return theItems.reduce(function(current, next) {
+      return (current.price * current.quantity)
+      + (next.price * next.quantity)
+    })
+  })
+}
+
+// orderSchema.virtual('sumPrices').get(function() {
+//   return this.getTotalPrice()
+// })
+
 
 mongoose.model('Order', orderSchema);
--- server/db/models/models.product.js
+++ server/db/models/models.product.js
@@ -8,25 +8,39 @@ var productSchema = new mongoose.Schema({
         unique: true
     },
     category: {
-        type: String
+        type: [String]
+        //TODO Add enum: [options]
     },
     price: {
-        type: Number
-    },
-    description: {
         type: String
     },
-    images: [{
-      type: Buffer,
-      extension: String,
-      select: false,
-      url: String
-    }],
+    description: {
+        type: String,
+        minlength: 20
+    },
+    images: [String],
     numRemaining: {
-      type: Number
+      type: Number,
+      default: 0
     }
 });
 
+//TODO Test this once we have a front end to test it on.
+productSchema.methods.getAverageRating = function()  {
+  var numReviews;
+  mongoose.model('Review').find({product: this._id})
+  .then(function(theReviews) {
+    numReviews = theReviews.length
+    return theReviews.reduce(function(current, next) {
+        return current.rating + next.rating;
+    })
+  })
+  .then(function(ratingsTotal) {
+    return ratingsTotal / length
+  })
+}
+
+// Won't work yet
 productSchema.pre('save', function (next) {
     var self = this;
     this.images.forEach(function(image, idx) {
