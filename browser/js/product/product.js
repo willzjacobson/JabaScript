58f6ed1777f470449caa100f1e447ad180b75cb1
Samuel Ballan <sgb4622@gmail.com>
2015-11-09 11:29:53 -0500

    We now have a method that can tell if the logged in user has already left a review.

--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -14,14 +14,27 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('ProductCtrl', function ($scope, $state, product, reviews) {
+app.controller('ProductCtrl', function ($scope, $state, product, reviews, Session) {
 	$scope.product = product;
     $scope.reviews = reviews
+    $scope.session = Session
 
+    console.log("Our session is: ", $scope.session)
     $scope.averageRating = function() {
         var total = reviews.reduce(function(current, next) {
                     return current.rating + next.rating
         })
         return total / reviews.length
     }
+
+     $scope.userHasReview = function() {
+        console.log("Reviews", $scope.reviews)
+        console.log("Session User", $scope.session.user)
+        if(!$scope.session.user) return -1;
+        return (reviews.filter(function(review) {
+            return review.user._id === $scope.session.user._id
+        })).length;
+
+     }
+    console.log($scope.userHasReview())
 });
